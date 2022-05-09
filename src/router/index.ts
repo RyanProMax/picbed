e15b66bf3b9b097ada5b3a782e6eb8f73ea12a9b
Ryan <627311610@qq.com>
2022-05-09 15:45:28 +0800

    feat: nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import nprogress from 'nprogress';
 import routes from '~pages';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+});
+
+router.beforeEach(() => {
+  nprogress.start();
+  return true;
+});
+
+router.afterEach(() => {
+  nprogress.done();
 });
 
 export default router;
