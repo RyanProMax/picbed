b979132fb14aac7085a05ae87a1335395a1d4482
Ryan <627311610@qq.com>
2022-05-09 17:51:51 +0800

    feat: upload file

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { ElMessage } from 'element-plus';
-import { checkUserConfig, fetchRepoFiles, getUserConfig } from '~/utils';
+import { ElButton, ElMessage, ElMessageBox } from 'element-plus';
+import { Plus } from '@element-plus/icons-vue';
+import { useFileSystemAccess } from '@vueuse/core';
+import { checkUserConfig, fetchRepoFiles, fetchUploadFile, getUserConfig } from '~/utils';
 
 const router = useRouter();
+const userConfig = getUserConfig();
 
 const loading = ref<boolean>(false);
 const list = ref<any>([]);
 
-onMounted(async() => {
-  const userConfig = getUserConfig();
-
+const fetchList = async() => {
   if (!checkUserConfig(userConfig)) {
     router.replace('/config');
     return;
@@ -28,15 +29,60 @@ onMounted(async() => {
       type: 'warning'
     });
   }
+};
+
+const { open, file } = useFileSystemAccess({
+  dataType: 'Blob',
+  types: [{
+    description: 'image',
+    accept: {
+      'image/*': ['.png', '.gif', '.jpeg', '.jpg']
+    }
+  }],
+  excludeAcceptAllOption: true
+});
+
+const confirmUpload = async(file: File, sha?: string) => {
+  await fetchUploadFile(file, userConfig, sha);
+  await fetchList();
+};
+
+const handleUpload = async() => {
+  loading.value = true;
+  await open();
+  const existFile = list.value.find((x: any) => x.name === file.value!.name);
+  if (existFile) {
+    ElMessageBox.confirm(
+      '已有同名文件，是否覆盖？',
+      '提示',
+      {
+        confirmButtonText: '确认覆盖',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }
+    )
+      .then(() => {
+        confirmUpload(file.value!, existFile.sha);
+      });
+  }
+  else {
+    await confirmUpload(file.value!);
+  }
+};
+
+onBeforeMount(() => {
+  fetchList();
 });
 </script>
 
 <template>
-  <div
-    v-loading="loading"
-    w:w="full" w:max-w="900px" w:min-h="480px" w:m="t-30px" w:grid="~ cols-4 <md:cols-2 <lg:cols-3 gap-4 auto-rows-auto"
-    w:overflow="hidden"
-  >
-    <img v-for="item in list" :key="item.name" :src="item.download_url" w:cursor="pointer">
+  <div v-loading="loading">
+    <div
+      w:w="full" w:max-w="900px" w:min-h="480px" w:m="t-30px"
+      w:grid="~ cols-4 <md:cols-2 <lg:cols-3 gap-4 auto-rows-auto" w:overflow="hidden"
+    >
+      <img v-for="item in list" :key="item.name" :src="`${item.download_url}?t=${Date.now()}`" w:cursor="pointer">
+    </div>
+    <ElButton :icon="Plus" circle type="danger" w:pos="fixed right-8 bottom-8" @click="handleUpload" />
   </div>
 </template>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,7 +14,7 @@ export const getUserConfig = () => {
 };
 
 export const checkUserConfig = (userConifg: UserConfig) => {
-  return Object.entries(userConifg).every(([key, value]) => key === 'PATH' || !!value);
+  return Object.entries(userConifg).every(([key, value]) => key === 'PATH' || !!value.value);
 };
 
 export const parseUserConfig = (userConifg: UserConfig) => {
@@ -38,4 +38,29 @@ export const fetchRepoFiles = async(userConifg: UserConfig) => {
     }
   });
   return (files || []).filter((x: any) => x.type === 'file');
+};
+
+export const fileToBase64 = (file: File) => new Promise<string>((resolve, reject) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => resolve(reader.result as string);
+  reader.onerror = error => reject(error);
+});
+
+/**
+ * 上传图片
+ */
+export const fetchUploadFile = async(file: File, userConifg: UserConfig, sha?: string) => {
+  const { account, repo, branch, path } = parseUserConfig(userConifg);
+  const content = await fileToBase64(file);
+  return callAPI({
+    url: `https://api.github.com/repos/${account}/${repo}/contents/${path}/${file.name}`,
+    method: 'put',
+    data: {
+      branch,
+      content: content.split('base64,')[1],
+      message: 'feat: upload by @RyanProMax/picbed',
+      sha
+    }
+  });
 };
