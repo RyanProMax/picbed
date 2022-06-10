0fdccb3a283379f65d11835bc3e5e313b571d231
Ryan <627311610@qq.com>
2022-06-10 22:13:04 +0800

    feat: picture previewer

---
+++ src/components/Previewer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ElImageViewer } from 'element-plus';
+const { urlList, onClose } = defineProps<{
+  urlList: string[]
+  onClose: () => void
+}>();
+</script>
+
+<template>
+  <ElImageViewer v-if="Boolean(urlList.length)" :url-list="urlList" @close="onClose()" />
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,12 +5,14 @@ import { ElButton, ElMessage, ElMessageBox } from 'element-plus';
 import { Plus } from '@element-plus/icons-vue';
 import { useFileSystemAccess } from '@vueuse/core';
 import { checkUserConfig, fetchRepoFiles, fetchUploadFile, getUserConfig } from '~/utils';
+import Previewer from '~/components/Previewer.vue';
 
 const router = useRouter();
 const userConfig = getUserConfig();
 
 const loading = ref<boolean>(false);
 const list = ref<any>([]);
+const urlList = ref<string[]>([]);
 
 const fetchList = async() => {
   if (!checkUserConfig(userConfig)) {
@@ -48,26 +50,38 @@ const confirmUpload = async(file: File, sha?: string) => {
 };
 
 const handleUpload = async() => {
-  loading.value = true;
-  await open();
-  const existFile = list.value.find((x: any) => x.name === file.value!.name);
-  if (existFile) {
-    ElMessageBox.confirm(
-      '已有同名文件，是否覆盖？',
-      '提示',
-      {
-        confirmButtonText: '确认覆盖',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }
-    )
-      .then(() => {
-        confirmUpload(file.value!, existFile.sha);
-      });
+  try {
+    loading.value = true;
+    await open();
+    const existFile = list.value.find((x: any) => x.name === file.value!.name);
+    if (existFile) {
+      ElMessageBox.confirm(
+        '已有同名文件，是否覆盖？',
+        '提示',
+        {
+          confirmButtonText: '确认覆盖',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      )
+        .then(() => {
+          confirmUpload(file.value!, existFile.sha);
+        });
+    }
+    else {
+      await confirmUpload(file.value!);
+    }
   }
-  else {
-    await confirmUpload(file.value!);
+  catch (err) {
+    console.log('err', err);
   }
+  finally {
+    loading.value = false;
+  }
+};
+
+const handlePreview = (index: number) => {
+  urlList.value = list.value.slice(index).concat(list.value.slice(0, index)).map((x: any) => x.download_url);
 };
 
 onBeforeMount(() => {
@@ -81,8 +95,12 @@ onBeforeMount(() => {
       w:w="full" w:max-w="900px" w:min-h="480px" w:m="t-30px"
       w:grid="~ cols-4 <md:cols-2 <lg:cols-3 gap-4 auto-rows-auto" w:overflow="hidden"
     >
-      <img v-for="item in list" :key="item.name" :src="`${item.download_url}?t=${Date.now()}`" w:cursor="pointer">
+      <img
+        v-for="(item, idx) in list" :key="item.name" :src="`${item.download_url}?t=${Date.now()}`" w:cursor="pointer"
+        @click="handlePreview(idx)"
+      >
     </div>
+    <Previewer :url-list="urlList" :on-close="() => (urlList = [])" />
     <ElButton :icon="Plus" circle type="danger" w:pos="fixed right-8 bottom-8" @click="handleUpload" />
   </div>
 </template>
